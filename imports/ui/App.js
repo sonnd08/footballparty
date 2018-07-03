60e5155b3f5e5133c1d633b01f824c3d23c29479
Son Nguyen Dinh <001.IcedTea@gmail.com>
2018-07-03 10:57:50 +0700

    using react as a template

---
+++ imports/ui/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+import Nav from './Nav.js';
+
+ 
+
+// App component - represents the whole app
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        test
+      </div>
+
+    );
+
+  }
+
+}
