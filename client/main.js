143b98ccfa610e99b72e60a68f7aa8f519bdedb6
Son Nguyen Dinh <001.IcedTea@gmail.com>
2018-07-06 18:18:08 +0700

    reac-router-dom

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 import './main.html';
 import App from '../imports/ui/App';
@@ -8,5 +10,9 @@ import App from '../imports/ui/App';
  
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'));
+  render(
+  <Router>
+    <Route path='/' component={App}/>
+  </Router>,
+  document.getElementById('root'));
 });
