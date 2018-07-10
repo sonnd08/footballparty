c04a56456aa826a6da8f91c90092ca1466544e87
Son Nguyen Dinh <001.IcedTea@gmail.com>
2018-07-10 10:47:27 +0700

    Change datepicker to slider

--- imports/ui/_Components/DatePicker.jsx
+++ imports/ui/_Components/DatePicker.jsx
@@ -1,14 +1,56 @@
 import React, {Component} from 'react';
 import AvatarAndName from './AvatarAndName';
+import Slider from 'react-slick';
 
 export default class Comment extends Component {
     render() {
+      var settings = {
+        arrows: true,
+        speed: 500,
+        slidesToShow: 16,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        infinite: true,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        className:"d-flex",
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 10,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 7,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
 
         return (
+          <div className='col-12'>
+        
             <div className="datePickerContainer">
                 <div className="datePicker">
-                    <div className="multiBtnContainer d-flex justify-content-center">
-                        <button className="button firstBtn">&lt;</button>
+                    <div className="multiBtnContainer">
+                      <Slider {...settings}>
+                        <button className="button passedDate">01</button>
+                        <button className="button passedDate">02</button>
+                        <button className="button passedDate">03</button>
+                        <button className="button passedDate">04</button>
+                        <button className="button passedDate">05</button>
                         <button className="button passedDate">06</button>
                         <button className="button passedDate">07</button>
                         <button className="button passedDate">08</button>
@@ -23,11 +65,47 @@ export default class Comment extends Component {
                         <button className="button commingDate">17</button>
                         <button className="button commingDate">18</button>
                         <button className="button commingDate">19</button>
-                        <button className="button lastBtn">&gt;</button>
+                        <button className="button commingDate">20</button>
+                        <button className="button commingDate">21</button>
+                        <button className="button commingDate">22</button>
+                        <button className="button commingDate">23</button>
+                        <button className="button commingDate">24</button>
+                        <button className="button commingDate">25</button>
+                        <button className="button commingDate">26</button>
+                        <button className="button commingDate">27</button>
+                        <button className="button commingDate">28</button>
+                        <button className="button commingDate">29</button>
+                        <button className="button commingDate">30</button>
+                        <button className="button commingDate">31</button>
+                      </Slider>
                     </div>
                 </div>
             </div>
+      </div>
 
         );
     }
+}
+
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <button 
+      className="button lastBtn"
+      style={{ ...style}}
+      onClick={onClick}
+    >&gt;</button>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <button 
+      className="button firstBtn" 
+      style={{ ...style}}
+      onClick={onClick} 
+    >&lt;</button>
+  );
 }
