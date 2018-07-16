d6ba1e7c307d4cadaa6090a3825345994cbe5062
Son Nguyen Dinh <001.IcedTea@gmail.com>
2018-07-16 17:12:05 +0700

    delete some cmt

--- imports/ui/_Components/DatePicker.jsx
+++ imports/ui/_Components/DatePicker.jsx
@@ -89,37 +89,6 @@ class DatePicker extends Component {
                   {today.get('date')}
                 </button>
                 {datesAfterBtn.map(btn => btn)}
-                {/* <button className="button passedDate">01</button>
-                  <button className="button passedDate">02</button>
-                  <button className="button passedDate">03</button>
-                  <button className="button passedDate">04</button>
-                  <button className="button passedDate">05</button>
-                  <button className="button passedDate">06</button>
-                  <button className="button passedDate">07</button>
-                  <button className="button passedDate">08</button>
-                  <button className="button currDate">09</button>
-                  <button className="button commingDate active">10</button>
-                  <button className="button commingDate">11</button>
-                  <button className="button commingDate">12</button>
-                  <button className="button commingDate">13</button>
-                  <button className="button commingDate">14</button>
-                  <button className="button commingDate">15</button>
-                  <button className="button commingDate">16</button>
-                  <button className="button commingDate">17</button>
-                  <button className="button commingDate">18</button>
-                  <button className="button commingDate">19</button>
-                  <button className="button commingDate">20</button>
-                  <button className="button commingDate">21</button>
-                  <button className="button commingDate">22</button>
-                  <button className="button commingDate">23</button>
-                  <button className="button commingDate">24</button>
-                  <button className="button commingDate">25</button>
-                  <button className="button commingDate">26</button>
-                  <button className="button commingDate">27</button>
-                  <button className="button commingDate">28</button>
-                  <button className="button commingDate">29</button>
-                  <button className="button commingDate">30</button>
-                  <button className="button commingDate">31</button> */}
               </Slider>
             </div>
           </div>
